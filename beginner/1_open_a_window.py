1364d55c69e6e9278ceff1cf97d0e9f30310d177
bigpollowo <124317193+BIGPOLLOWO@users.noreply.github.com>
2024-01-28 17:14:34 -0600

    Update and rename 1.open_a_window.py to 1_open_a_window.py

--- beginner/1_open_a_window.py
+++ beginner/1_open_a_window.py
@@ -12,3 +12,19 @@ while running:
     if event.type == pygame.QUIT: # This, constantly checks if you click on [x] (topright of your pygame window)
       pygame.quit()               # if you did, quits pygame, and running will be False.
       running = False             # This enables the user to close the window.
+
+
+""" 
+# this is another common way to setup your program
+import pygame, sys  # or -> from sys import exit as sys_exit
+
+pygame.init()
+screen = pygame.display.set_mode((400,300))
+pygame.display.set_caption('Opening a pygame window')
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: 
+      pygame.quit()               
+      sys.exit()
+"""
